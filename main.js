94c042907a50092a5078fa551c73acae4a90674b
leonardosf98 <leonardo-f98@hotmail.com>
2023-08-27 11:46:42 -0300

    primeira versão site novo

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ let message = document.querySelector(".message-input");
 sendButton.addEventListener("click", function () {
     let messageText = message.value;
     });
+
+let presentation = document.querySelector(".main-content");
+let presentationText = "Sou desenvolvedor web e nutricionista!"
+let i = 0;
+function typewrite(){
+        if (i < presentationText.length) {
+          presentation.innerHTML += presentationText.charAt(i);
+          i++;
+          setTimeout(typewrite, 50);
+        }
+      }
+    typewrite();

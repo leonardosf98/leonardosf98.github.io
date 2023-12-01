a5ce163604fe6238e1060c7c28e50d7591631470
leonardosf98 <leonardo-f98@hotmail.com>
2023-12-01 10:13:14 -0300

    adição do carrossel

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let presentation = document.querySelector(".main-content");
-let presentationText = "Sou desenvolvedor web e nutricionista!"
 let i = 0;
-function typewrite(){
-        if (i < presentationText.length) {
-          presentation.innerHTML += presentationText.charAt(i);
-          i++;
-          setTimeout(typewrite, 50);
-        }
-      }
-    typewrite();
+function typewrite() {
+  let presentation = document.querySelector(".main-content");
+  let presentationText = "Sou desenvolvedor web e nutricionista!";
+  if (i < presentationText.length) {
+    presentation.innerHTML += presentationText.charAt(i);
+    i++;
+    setTimeout(typewrite, 50);
+  }
+}
+typewrite();

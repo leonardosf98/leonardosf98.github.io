ede653adde6ce123f1a782f09a456e92cf75a92b
Leonardo Souza <leonardosouza@TARS.local>
2024-02-21 20:45:08 -0300

    fix: burguer not opening issue

--- src/components/HeaderSimple/HeaderSimple.tsx
+++ src/components/HeaderSimple/HeaderSimple.tsx
@@ -1,5 +1,13 @@
-import { Container, Group, Text, ActionIcon, rem } from '@mantine/core';
-
+import {
+  Container,
+  Group,
+  Text,
+  ActionIcon,
+  rem,
+  Menu,
+  Burger,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import classes from './HeaderSimple.module.css';
 import {
   IconDeviceDesktopCode,
@@ -14,6 +22,8 @@ const links = [
 ];
 
 export function HeaderSimple() {
+  const [opened, { toggle }] = useDisclosure(false);
+
   const items = links.map((link) => (
     <a key={link.label} href={link.link} className={classes.link}>
       {link.label}
@@ -60,6 +70,22 @@ export function HeaderSimple() {
               stroke={1.5}
             />
           </ActionIcon>
+
+          <Menu shadow="md" width={200}>
+            <Menu.Target>
+              <Burger
+                opened={opened}
+                onClick={toggle}
+                hiddenFrom="xs"
+                size="sm"
+              />
+            </Menu.Target>
+            <Menu.Dropdown>
+              {items.map((item) => {
+                return <Menu.Item>{item}</Menu.Item>;
+              })}
+            </Menu.Dropdown>
+          </Menu>
         </Group>
       </Container>
     </header>

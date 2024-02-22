7359beae2d77cba4bbdda75cca9a12aab4e3ba47
leonardosf98 <leonardo-f98@hotmail.com>
2024-02-22 10:16:24 -0300

    feat: add transition

--- src/components/HeroBullets/HeroBullets.tsx
+++ src/components/HeroBullets/HeroBullets.tsx
@@ -9,57 +9,59 @@ import {
   ThemeIcon,
   rem,
   Transition,
-} from '@mantine/core';
-import { IconCheck } from '@tabler/icons-react';
-import classes from './HeroBullets.module.css';
-import { useToggle } from '@mantine/hooks';
-import programming from './programming.svg';
-import interview from './interview.svg';
-import { useState } from 'react';
+} from "@mantine/core";
+import { IconCheck } from "@tabler/icons-react";
+import classes from "./HeroBullets.module.css";
+import { useToggle } from "@mantine/hooks";
+import programming from "./programming.svg";
+import interview from "./interview.svg";
+import { useState } from "react";
 
 export function HeroBullets() {
   const nutrition = {
-    title: 'Abordagem',
-    titleSpan: 'humana e individual',
-    titleLastPart: 'da nutrição',
+    title: "Abordagem",
+    titleSpan: "humana e individual",
+    titleLastPart: "da nutrição",
     subtitle:
-      ' Escuta ativa, foco no cliente e trabalho em equipe são algumas das habilidades que desenvolvi ao longo da minha carreira como nutricionista clínico',
-    bulletOneTitle: 'Formação',
-    bulletOneContent: '- Graduado na Universidade Federal de São Paulo',
-    bulletTwoTitle: 'Educação',
+      " Escuta ativa, foco no cliente e trabalho em equipe são algumas das habilidades que desenvolvi ao longo da minha carreira como nutricionista clínico",
+    bulletOneTitle: "Formação",
+    bulletOneContent: " - Graduado na Universidade Federal de São Paulo",
+    bulletTwoTitle: "Educação",
     bulletTwoContent:
-      '– Meu trabalho como nutricionista envolve educaros pacientes sobre as escolhas que eles fazem',
-    bulletThreeTitle: 'Trabalho em equipe',
+      " – Meu trabalho como nutricionista envolve educaros pacientes sobre as escolhas que eles fazem",
+    bulletThreeTitle: "Trabalho em equipe",
     bulletThreeContent:
-      'Muitas vezes a atuação envolve contato com outros profissionais da área da saúde',
+      " - Muitas vezes a atuação envolve contato com outros profissionais da área da saúde",
     image: interview,
-    buttonOneState: 'default',
-    buttonTwoState: 'filled',
+    buttonOneState: "default",
+    buttonTwoState: "filled",
   };
   const tech = {
-    title: 'Um novo ',
-    titleSpan: 'desafio',
-    titleLastPart: 'em uma nova área',
+    title: "Um novo ",
+    titleSpan: "desafio",
+    titleLastPart: "em uma nova área",
     subtitle:
-      'Sempre fui muito curioso quanto ao funcionamento do computador que tive desde pequeno em casa, meus pais odiavam quando eu o desmontava. Hoje me apoiam na carreira.',
-    bulletOneTitle: 'Formação',
+      "Sempre fui muito curioso quanto ao funcionamento do computador que tive desde pequeno em casa, meus pais odiavam quando eu o desmontava. Hoje me apoiam na carreira.",
+    bulletOneTitle: "Formação ",
     bulletOneContent:
-      '- Graduando em Sistemas para Internet na Faculdade de Tecnologia do Estado de São Paulo (FATEC)',
-    bulletTwoTitle: 'Facilidade em ensinar',
+      " - Graduando em Sistemas para Internet na Faculdade de Tecnologia do Estado de São Paulo (FATEC)",
+    bulletTwoTitle: "Facilidade em ensinar",
     bulletTwoContent:
-      '- Queria passar o que sei para os outros e minha primeira oportunidade foi na monitoria de lógica de programação da faculdade.',
-    bulletThreeTitle: 'Trabalho em Equipe',
+      " - Queria passar o que sei para os outros e minha primeira oportunidade foi na monitoria de lógica de programação da faculdade.",
+    bulletThreeTitle: "Trabalho em Equipe",
     bulletThreeContent:
-      '- Colaborei em projetos de desenvolvimento de software, interagindo com colegas e profissionais para alcançar soluções eficientes e inovadoras.',
+      " - Colaborei em projetos de desenvolvimento de software, interagindo com colegas e profissionais para alcançar soluções eficientes e inovadoras.",
     image: programming,
-    buttonOneState: 'filled',
-    buttonTwoState: 'default',
+    buttonOneState: "filled",
+    buttonTwoState: "default",
   };
   const handleClick = () => {
     setShow(false);
     console.log(show);
     toggle();
-    setShow(true);
+    setTimeout(() => {
+      setShow(true);
+    }, 200);
   };
 
   const [info, toggle] = useToggle([tech, nutrition]);
@@ -69,17 +71,18 @@ export function HeroBullets() {
     <Container size="lg">
       <Transition
         mounted={show}
-        transition="fade"
-        duration={400}
+        transition="slide-right"
+        duration={300}
         timingFunction="ease"
+        keepMounted
       >
         {(styles) => {
           return (
             <div style={styles} className={classes.inner} id="about">
               <div className={classes.content}>
                 <Title className={classes.title}>
-                  {info.title}{' '}
-                  <span className={classes.highlight}>{info.titleSpan}</span>{' '}
+                  {info.title}{" "}
+                  <span className={classes.highlight}>{info.titleSpan}</span>{" "}
                   <br /> {info.titleLastPart}
                 </Title>
                 <Text c="dimmed" mt="md">
